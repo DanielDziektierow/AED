0a59e4c6ca4ec7de7b5dbbb8afc3d9f95f7b020a
DanielDziektierow <dz09dz@hotmail.com>
2020-09-06 12:51:46 -0300

    v1

---
+++ 09-Color/Color.h
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cassert>
+#include <string>
+#include <array>
+#include <cstdint>
+
+using namespace std;
+//const
+const unsigned MAX_COLOR=3;
+const Color ROJO={255,0,0};
+const Color AZUL={0,0,255};
+const Color AMARILLO={255,255,0};
+const Color VERDE={0,255,0};
+const Color NARANJA={255,170,0};
+const Color VIOLETA={140,0,255};
+const Color NEGRO={0,0,0};
+const Color BLANCO={255,255,255};
+
+//Structs
+using Colores= std::array <uint8_t,MAX_COLOR>;
+
+struct Color{
+	Colores col;
+};
+
+
+
