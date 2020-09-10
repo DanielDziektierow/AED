994591445864f15343c9f9477b8afb20dd756c89
DanielDziektierow <dz09dz@hotmail.com>
2020-09-10 18:02:46 -0300

    v2.0

--- 09-Color/Color.cpp
+++ 09-Color/Color.cpp
@@ -9,13 +9,20 @@
 #include "Color.h"
 
 int main(){
-	Color c1={252,111,123}, c2={10,10,10}, comp;
+	Color c1={252,111,123}, c2={10,10,10}, comp, suma, resta;
 	string hexa="", rgb="";
 	
 	c1=Mezclar(c1,c2);
 	hexa=GetHtmlHex(c1);
 	rgb=GetHtmlRGB(c1);
 	comp=GetComplementario(c1);
+	suma=Sumar(c1, c2);
+	resta=Restar(c1, c2);
+	CrearSvgConTextoEscritoEnAltoContraste(hexa,rgb,c2);
+	
+	assert(ROJO.col.at(0) == 255);
+	assert(AZUL.col.at(2) == 255);
+	assert(AMARILLO.col.at(1) == 255);
 	
 	assert(c1.col.at(0) == 127);
 	assert(c1.col.at(1) == 60);
@@ -28,5 +35,7 @@ int main(){
 	assert(comp.col.at(1)==195);
 	assert(comp.col.at(2)==189);
 	
+	assert(suma.col.at(0) == 137);
+	assert(resta.col.at(0) == 117);
 }
 
