be0ffd95ca88309d4852b831ff25d1ae2c026852
Daniel Dziektierow <danield@ctrl365.com>
2020-09-19 14:53:32 -0300

    v0.1

--- 11-Punto/Punto.h
+++ 11-Punto/Punto.h
@@ -14,32 +14,53 @@
 
 using namespace std;
 
+//Constantes
+#define PI 3.14159265
+
 struct Punto{
 	int x, y;
 };
 
 //Prototipos
 
-int GetPhi(Punto , Punto );
-unsigned GetHip(unsigned, unsigned);
+int GetPhi(Punto , Punto);
+int GetRho(Punto, Punto);
+unsigned GetCuadrante(Punto);
+//GetEje();
+//GetSemiPlano();
+unsigned GetHip(int, int);
 void MoverPunto(Punto &, int, int);
 
 //funciones
 
-unsigned GetHip(Punto cat1, Punto cat2){
+unsigned GetHip(int distx, int disty){
 	unsigned hip, aux_x, aux_y;
-	aux_x=cat2.x-cat1.x;
-	aux_y=cat2.y-cat1.y;
-	hip=pow(aux_x,2)+pow(aux_y,2);
+	hip=pow(distx,2)+pow(disty,2);
 	return hip;
 }
 
-/*int GetPhi(Punto p1, Punto p2){
-	int disty, distx,phi, hip;
-	disty=p2.y-p1.y;
-	distx=p2.x-p1.x;
-	hip= GetHip(disty,distx);
-	sin(phi)=(p2.y-p1.y)/ hip;
-	return phi;
+int GetPhi(Punto p1, Punto p2){		//Respecto del origen
+	unsigned dist1, dist2;
+	dist1= GetHip(p1.x, p1.y);
+	dist2= GetHip(p2.x, p2.y);
+	return dist1 < dist2 ? acos(dist1/dist2) : acos(dist2/dist1);	 //debe ser en radianes
 }
-*/
+
+int GetRho(Punto p1, Punto p2){		//Respecto del origen
+	unsigned dist1, dist2;
+	dist1= GetHip(p1.x, p1.y);
+	dist2= GetHip(p2.x, p2.y);
+	return dist1 < dist2 ? asin(dist1/dist2) : acos(dist2/dist1);	 //debe ser en radianes
+}
+
+unsigned GetCuadrante(Punto p){
+	if (p.x>0 and p.y>0) return 1;
+	if (p.x<0 and p.y>0) return 2;
+	if (p.x<0 and p.y<0) return 3;
+	if (p.x>0 and p.y<0) return 4;
+}
+
+void MoverPunto(Punto & p, int x, int y){
+	p.x=p.x + x;
+	p.y= p.y + y;
+}
