4a8f74dcc5f715e8320ad221a633218152d54ee6
DanielDziektierow <danield@ctrl365.com>
2020-12-10 19:24:52 -0300

    Final 0.2

---
+++ 10-Poligono/includes/Estructuras.h
@@ -0,0 +1,26 @@
+/*Materia: Algoritmos y Estructura de Datos
+*Curso: K1051
+*Anio y Cuatrimestre: 2020 1er Cuat.
+*Legajo: 1599872
+*Apellido: Dziektierow
+*Nombre: Daniel
+*/
+#ifndef ESTRUCTURAS_H
+#define ESTRUCTURAS_H
+using namespace std;
+#pragma once
+#include <iostream>
+#include <string>
+#include <array>
+#include <cstdint>
+#include <fstream>
+#include <cassert>
+#include <cstdio>
+
+#include "Color.h"
+#include "Punto.h"
+
+
+
+
+#endif
